af8bc07190b441da2c2581d293dcabcfac6c767a
ROHIT SHARMA <RohitSharma.creator@gmail.com>
2025-11-09 04:33:40 +0000

    fix this error kipping validation of types    Skipping linting    Collec

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -29,9 +28,6 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
-  },
-  devIndicators: {
-    buildActivity: false,
   },
   experimental: {
     allowedDevOrigins: [
